e7ffeb24bc652dd263151454b97072b5c6be5980
ivanistrator <peregoedov20@gmail.com>
2024-08-04 20:02:58 +0700

    Spring initialization

---
+++ app-dir/src/main/java/com/peregoedov/tasker/app/app_dir/AppDirApplication.java
@@ -0,0 +1,13 @@
+package com.peregoedov.tasker.app.app_dir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDirApplication.class, args);
+	}
+
+}
---
+++ app-dir/src/test/java/com/peregoedov/tasker/app/app_dir/AppDirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peregoedov.tasker.app.app_dir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
