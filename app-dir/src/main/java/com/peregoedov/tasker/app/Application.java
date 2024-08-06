04f591f8ebf5d32c5f735a49bfa57735544c7c0e
ivanistrator <peregoedov20@gmail.com>
2024-08-06 16:42:53 +0700

    add mainApplication file

--- app-dir/src/main/java/com/peregoedov/tasker/app/Application.java
+++ app-dir/src/main/java/com/peregoedov/tasker/app/Application.java
@@ -1,13 +1,13 @@
-package com.peregoedov.tasker.app.app_dir;
+package com.peregoedov.tasker.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppDirApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppDirApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
